0565dce82121e504b32d082f2e76756e9a81e7f4
Devina Ekawati <devina.ekawati@gmail.com>
2015-04-24 02:46:45 +0700

    JLayerePane

--- src/mr/angkot/Angkot.java
+++ src/mr/angkot/Angkot.java
@@ -6,11 +6,10 @@ package mr.angkot;
  * and open the template in the editor.
  */
 
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.*;
-import javax.swing.JComponent;
-import java.awt.Rectangle;
+import javax.swing.*;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -27,22 +26,30 @@ public class Angkot extends JComponent {
   private int y;
   
   public Angkot() {
+    setLayout(null);
     passengers = new ArrayList();
     capacity = 14;
     color = "Yellow";
     x = 10;
-    y = 10;
+    y = 100;
   }
   
-  public void move() {
-    /*if (x == 110) {
-      try {
-        TimeUnit.MILLISECONDS.sleep(500);
-      } catch (InterruptedException ex) {
-        Logger.getLogger(Angkot.class.getName()).log(Level.SEVERE, null, ex);
-      }
-    }*/
-    x += 50;
+  public Angkot(int _x, int _y) {
+    setLayout(null);
+    passengers = new ArrayList();
+    capacity = 14;
+    color = "Yellow";
+    x = _x;
+    y = _y;
+  }
+  
+  public void move(boolean inX, boolean inY) {
+    if (inX) {
+     x += 50;
+    }
+    else if (inY) {
+      y += 50;
+    }
     repaint();
   }
   
@@ -55,16 +62,32 @@ public class Angkot extends JComponent {
   }
   
   @Override
-  public void paint (Graphics g) {
-    g.setColor(Color.BLUE);
-    g.fillRect (x, y, 100, 50);
+  public void paintComponent (Graphics g) {
+    Graphics2D g2d = (Graphics2D) g.create();
+    super.paintComponent(g2d);
+    g2d.setColor(Color.BLUE);
+    g2d.fillRect (x, y, 100, 50);
     
     try {
       TimeUnit.MILLISECONDS.sleep(500);
     } catch (InterruptedException ex) {
       Logger.getLogger(Angkot.class.getName()).log(Level.SEVERE, null, ex);
     }
-    move();
+    boolean inX = true;
+    boolean inY = false;
+    if ((x >= 1200) && (y <= 500)) {
+      g2d.clearRect(x, y, 100, 50);
+      AffineTransform transform = new AffineTransform();
+      transform.rotate(Math.PI/2, x + 100/2, y + 50/2);
+      AffineTransform old = g2d.getTransform();
+      g2d.transform(transform);
+      g2d.setColor(Color.BLUE);
+      g2d.fillRect (x, y, 100, 50);
+      g2d.setTransform(old);
+      inX = false;
+      inY = true;
+    }
+    move(inX, inY);
   } 
 }
 
--- src/mr/angkot/MrAngkot.java
+++ src/mr/angkot/MrAngkot.java
@@ -5,26 +5,61 @@
  */
 package mr.angkot;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 /**
  *
  * @author Devina
  */
-public class MrAngkot {
+public class MrAngkot extends JFrame {
+  
+  public MrAngkot() {
+    super("Mr. Angkot !");
+    setSize(1366, 730);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    JLayeredPane lp = getLayeredPane();
+
+    // Create terminal 1 (ntar d taronya d kelas terminal)
+    JLabel terminal1 = new JLabel("Terminal 1");
+    terminal1.setOpaque(true);
+    terminal1.setBackground(Color.red);
+    terminal1.setBounds(20, 20, 100, 50);
+    
+    JLabel terminal2 = new JLabel("Terminal 2");
+    terminal2.setOpaque(true);
+    terminal2.setBackground(Color.green);
+    terminal2.setBounds(1200, 20, 100, 50);
+    
+    JLabel terminal3 = new JLabel("Terminal 3");
+    terminal3.setOpaque(true);
+    terminal3.setBackground(Color.pink);
+    terminal3.setBounds(1200, 600, 100, 50);
+    
+    /* JLabel bottom = new JLabel();
+    bottom.setBackground(Color.black);
+    bottom.setBounds(60, 60, 50, 50); */
+    
+    Angkot a1 = new Angkot();
+    a1.setBounds(0, 0, 1366, 730);
+    
+    // Place the buttons in different layers
+    
+    lp.add(a1, new Integer(1));
+    lp.add(terminal1, new Integer(2));
+    lp.add(terminal2, new Integer(3));
+    lp.add(terminal3, new Integer(4));
+  }
   
   /**
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     // TODO code application logic here
-    JFrame window = new JFrame();
-    Angkot a = new Angkot();
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setBounds(0, 0, 1366, 730);
-    window.add(a);
-    window.setVisible(true);
-    
+
+    MrAngkot sl = new MrAngkot();
+    sl.setVisible(true);
   }
   
 }
