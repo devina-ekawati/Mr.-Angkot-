bfaff3fa02496ccbd7bd1beb51511a11fcb90493
angelynz95 <angel.lynz95@gmail.com>
2015-04-30 10:55:08 +0700

    Finish

---
+++ test/mr/angkot/UserInterfaceTest.java
@@ -0,0 +1,48 @@
+/*
+ * @author Irene Wiliudarsan	(13513002)
+ * @author Angela Lynn		(13513032)
+ * @author Devina Ekawati	(13513088)
+ * @bug Tidak ada bug ditemukan
+ */  
+
+package mr.angkot;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Windows7
+ */
+
+
+public class UserInterfaceTest {
+  
+  public UserInterfaceTest() {
+  }
+  
+  @BeforeClass
+  public static void setUpClass() {
+  }
+  
+  @AfterClass
+  public static void tearDownClass() {
+  }
+  
+  @Before
+  public void setUp() {
+  }
+  
+  @After
+  public void tearDown() {
+  }
+
+  @Test
+  public void testSomeMethod() {
+  }
+  
+}
